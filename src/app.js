d783e3fd47e3d5b0ff42d4d900068d02ec9e3e65
Thomas Henrique Fideles Ribeiro <30496973@vli-logistica.com.br>
2020-04-30 15:35:17 -0300

    Precjetc Crud Base Finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,83 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+   return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const {id} = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find(repo => repo.id === id);
+
+  if(!repository)
+  {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+ 
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const {id} = request.params;
+  const repositoryIndexDelete = repositories.findIndex(repository => repository.id === id);
+ 
+  if(repositoryIndexDelete < 0)
+  {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+
+  repositories.splice(repositoryIndexDelete,1);
+
+  return response.status(204).send();
+
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const {id} = request.params;
+  
+  const repository = repositories.find(repo => repo.id === id);
+
+  if(!repository)
+  {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
+ 
+
 });
 
 module.exports = app;
